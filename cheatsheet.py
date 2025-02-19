6181aa02f1963ae39b2b48ca6f386e954fc3acf7
Gael Alejandro Fonseca <gfon@csu.fullerton.edu>
2025-02-19 15:12:45 -0800

    cheat sheet

---
+++ cheatsheet.py
@@ -0,0 +1,25 @@
+# Example 1: Iterating over a list
+fruits = ["apple", "banana", "cherry"]
+for fruit in fruits:
+    print(fruit)  # Outputs each fruit on a new line
+
+# Example 2: Using a for loop with a range
+for i in range(5):
+    print(i)  # Prints numbers 0 to 4
+
+# Example 1: Add an integer to the list
+lst = [1, 2, 3, 4]
+lst.append(5)
+print(lst) # Prints [1, 2, 3, 4, 5]
+
+
+# Example 1: List of integers
+lst = [4, 2, 1, 3]
+lst.sort()
+print(lst) # Prints [1, 2, 3, 4]
+
+
+# Example 2: List of strings
+lst = ['b', 'a', 'd', 'c']
+lst.sort()
+print(lst) # Prints ['a', 'b', 'c', 'd']
