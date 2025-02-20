b42286cef546e8113ed38b88455db18f14284d6f
Gael Alejandro Fonseca <gfon@csu.fullerton.edu>
2025-02-19 17:32:55 -0800

    probkem 10

--- Unit1/Session1/ProblemSet1/problem7.py
+++ Unit1/Session1/ProblemSet1/problem7.py
@@ -7,3 +7,13 @@ def get_lst(lst):
 
 print(get_lst([1, 2, 3]))
 print(get_lst([]))
+
+def get_last(lst):
+  if not lst:
+    return
+  else:
+    last = lst[-1]
+    return last
+
+print(get_last([1 , 3 , 4]))
+print(get_last([]))
