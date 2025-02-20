7d533dd7864b73536980f67651ca979bf8327911
Gael Alejandro Fonseca <gfon@csu.fullerton.edu>
2025-02-19 16:33:48 -0800

    problem4

---
+++ Unit1/Session1/ProblemSet1/problem4.py
@@ -0,0 +1,11 @@
+
+def sum(a, b):
+    return a + b
+
+def product(a,b):
+    return a*b
+
+print("The sum of 13 and 17 are: ") 
+print(sum(13, 17))
+print("The product of these two numbers are ")
+print(product(13, 17))
