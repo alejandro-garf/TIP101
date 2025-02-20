5d856f6ff88528e305ccfed16a099b171aef9b85
Gael Alejandro Fonseca <gfon@csu.fullerton.edu>
2025-02-19 16:38:10 -0800

    problems 4  & 5

--- Unit1/Session1/ProblemSet1/problem45.py
+++ Unit1/Session1/ProblemSet1/problem45.py
@@ -5,7 +5,15 @@ def sum(a, b):
 def product(a,b):
     return a*b
 
+def dbl(a):
+    return a * 2
+
 print("The sum of 13 and 17 are: ") 
 print(sum(13, 17))
+
+print("Doubling the output is: ")
+print(dbl(sum(13, 17)))
+
+
 print("The product of these two numbers are ")
 print(product(13, 17))
