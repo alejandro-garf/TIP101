3cabf47a35729f444f36f3b880e20bda49be6d5e
Gael Alejandro Fonseca <gfon@csu.fullerton.edu>
2025-02-24 14:35:49 -0800

    problem2

--- Unit1/Session1/ProblemSet2/Problem1.py
+++ Unit1/Session1/ProblemSet2/Problem1.py
@@ -1,5 +1,5 @@
 def greet_user(name):
-    return print("Hello " + name)
+    print("Hello " + name)
 
-Username = input("Hello User, what is your name?: ")
-greet_user(Username)
+username = input("Hello User, what is your name?: ")
+greet_user(username)
---
+++ Unit1/Session1/ProblemSet2/Problem2.py
@@ -0,0 +1,5 @@
+def difference(a, b):
+    return abs(a - b)
+
+diff = difference(3, 8)
+print(diff)
