11a7412408389436d748b8f550a4208bf720331b
Gael Alejandro Fonseca <gfon@csu.fullerton.edu>
2025-03-12 15:28:53 -0700

    unit 2

---
+++ Unit2/cheatsheet.py
@@ -0,0 +1,30 @@
+
+## BUILT IN FUNCTIONS
+
+## Type Casting
+
+## int(x) casts x as an integer
+
+x = 2.5
+print(x)
+
+x = int(x)
+print(x)
+
+### Same thing with floats and strings 
+
+# Remainder Divisions
+# % is also known as the modulus operator
+
+print(5 % 2)
+
+### Sum, max and min you already know
+
+ ### DICTIONARIES
+d['d'] = 4          # Adds a new key 'd' with value 4
+print(d)            # Outputs: {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+
+d['a'] = 100        # Updates the value of key 'a'
+print(d)            # Outputs: {'a': 100, 'b': 2, 'c': 3, 'd': 4}
+
+## Acessing elements
